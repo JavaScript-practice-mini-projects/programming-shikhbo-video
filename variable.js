f7c12b3ead9a094743dcf6c9602d6523b0257434
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-25 20:37:29 +0600

    variable first part

---
+++ variable.js
@@ -0,0 +1,16 @@
+/**
+ * je data store kore take variable bole.
+ * variable ke ekta box er moto chinta kora jete pare jekhane ekta kichu rakha jay.
+ * variable var diye lekha hoy.
+ * variable ke ekta nam deya hoy.
+ * js line sheshe ";" dite hoy. ata fullstop er moto kaj kore. 
+ * note: variable change hoteo pare abar naw hote pare.
+ */
+
+// akhne ekta variable lekha holo.
+var eggPrice = 40;
+var age = 22;
+
+/**
+ * akhane eggPrice and age holo variable er nam. ai variable gulo use kote hole amader nam dore call korte hobe.
+ */
