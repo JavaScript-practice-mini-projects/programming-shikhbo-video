a75b3db70ae7d03c5a18dda1152232d94b81269c
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-25 20:49:32 +0600

    variable part 2

--- variable.js
+++ variable.js
@@ -14,3 +14,26 @@ var age = 22;
 /**
  * akhane eggPrice and age holo variable er nam. ai variable gulo use kote hole amader nam dore call korte hobe.
  */
+
+50
+50
+50 
+// avabe jodi thake tahole bujte parbo na agulo asole ki kintu
+
+var sugarPrice = 50;
+var saltPrice = 50;
+var age = 50;
+
+// akhon kintu amra clearly bulte partechi konta diye ki bujayche so variable er name diye amara bujte pari kontar kon data store kore ache.
+
+
+// Variable er nam deyar niyom
+
+// var 1eggPrice = 33; 
+// avabe vul. age kokhonoi number deya jabe na. kintu
+// var eggPrice1 = 22;
+// avabe lekha jabe. tar mane age number use kora jabe na kintu pore use kora jabe.
+
+//achara variable er age kono spacial character use kora jay na kintu $ and _ use kora jay kintu ata recommendation na.
+
+// var $eggPrice = 22; var _eggPrice = 44; 
