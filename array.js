bc98089c84e9153c89b2f6470af8fb5e247a3098
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-26 20:44:53 +0600

    array class 2nd

--- array.js
+++ array.js
@@ -7,3 +7,20 @@
 // var publics = [33, 44, 55, 66, 77, 88, 99, 11];
 // var friendsAge = [12, 35, 64, 74, 74, 23, 23];
 // var tools = ["figma", "html", "css", "tailwind", "git"];
+
+// var price = [22,33,44,55, 88];
+// var name = ['hero', 'kk', 'jj', 'dd'];
+// var boolean = [true, false, "true", "false"];
+// console.log(boolean.length);
+
+// var toolsName = ["html", "css", "js", "tailwind"];
+// // console.log(toolsName.indexOf('js'));
+// var num = [22, 44, 66, 77, 75, 23];
+// // console.log(num.indexOf(77));
+
+// var x = toolsName.indexOf("tailwind");
+// console.log(toolsName[x])
+
+// var num = [22, 44, 66, 77, 75, 23];
+// var numValue = num[2];
+// console.log(numValue);
