b845f2c6ff46a70b35ac0347ecc88b6679614bc1
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-28 20:29:21 +0600

    while loop compalte

---
+++ while-loop.js
@@ -0,0 +1,19 @@
+// let count = 0;
+
+// while(count < 100){
+//     console.log(`I love you Fency ${count}` );
+//     count++;
+// }
+
+// let score = 10;
+// while(score > 0){
+//     console.log(score);
+//     score--;
+// }
+
+let fv = ["rupa", 'ontora', 'sathi', 'nilima', 'fency'];
+let count = 0;
+while(count < fv.length){
+    console.log(fv[count]);
+    count++;
+}
