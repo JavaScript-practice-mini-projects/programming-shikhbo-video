cd797f63d63530d82d899f8ef6067b3961e89a22
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-28 21:36:50 +0600

    switch case complate

---
+++ switch-case.js
@@ -0,0 +1,15 @@
+// let count = 0;
+
+// switch(count){
+//     case 0:
+//         console.log('Zero');
+//         break;
+//     case 1:
+//         console.log('One');
+//         break;
+//     case 2:
+//         console.log('Two');
+//         break;
+//     default:
+//         console.log('not found')
+// }
