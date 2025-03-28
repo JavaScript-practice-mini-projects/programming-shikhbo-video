4dd80c7206d02617a9b8f2c00a9de3b4637a3d02
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-28 17:19:56 +0600

    parseInt parceFloat & toFixed

---
+++ conversion.js
@@ -0,0 +1,32 @@
+// let num1 = "20";
+// let num2 = 30;
+
+// console.log(num1 + num2)
+
+// num1 = parseInt(num1);
+// console.log(num1 + num2)
+
+// let num1 = "20.66";
+// let num2 = "8.99";
+
+// num1 = parseInt(num1);
+// num2 = parseInt(num2)
+// console.log(num1, num2)
+
+// let num1 = "20.66";
+// let num2 = "8.99";
+// num2 = parseFloat(num2)
+// console.log(num2)
+
+// let num1 = "4.5";
+// let num2 = "4.50";
+// num1 = parseFloat(num1);
+// num2 = parseFloat(num2)
+// console.log(num1 + num2)
+
+// let num1 = 4.222;
+// let num2 = 5.999;
+
+// let result = num1.toFixed(2);
+// result = parseFloat(result)
+// console.log(result + 5)
