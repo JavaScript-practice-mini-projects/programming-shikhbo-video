d9ec16ca171f8e44e7370a14d0ddd4ca83f1bf09
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-28 20:50:20 +0600

    for loop complate

---
+++ for-loop.js
@@ -0,0 +1,29 @@
+// let count = 1;
+// while(count < 20){
+//     console.log(count);
+//     count += 2;
+// }
+
+
+
+// for(let count = 0; count < 20; count++){
+//     console.log(count);
+// }
+
+// for( let index = 10; index > 0; index--){
+//     console.log(index);
+// }
+
+// let fv = ['rupa', 'ontora', 'sathi', 'nilima', 'fency'];
+
+// for(let i = 0; i < fv.length; i++){
+//     console.log(fv[i]);
+// }
+
+// for(let i = 0; i < 10; i++){
+//     if( i === 5){
+//         console.log(' 5 is detected ');
+//         break;
+//     }
+//     console.log(i)
+// }
