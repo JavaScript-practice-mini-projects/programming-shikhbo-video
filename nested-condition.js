270271bbbd8787d922849374fa51cc10f08fa001
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-28 20:16:06 +0600

    nested condition

---
+++ nested-condition.js
@@ -0,0 +1,11 @@
+// let a = 5;
+// let b = 6;
+// let c = 7;
+
+// if(a < b){
+//     if(b < c){
+//         console.log('condition is true')
+//     }
+// }else{
+//     console.log('A is grater than B')
+// }
