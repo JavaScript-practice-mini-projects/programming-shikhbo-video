0dce0c325b6e7f7a35d597f6e1d9dfefa3996b13
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-28 21:20:00 +0600

    do while loop complate

---
+++ do-while-loop.js
@@ -0,0 +1,14 @@
+// let count = 1;
+// do{
+//     console.log('loop is running');
+//     count++;
+// }while(count <= 10);
+
+// let count = 0;
+// let num = '';
+// while(count < 10){
+//     // num =  Math.random() * Math.random();
+//     console.log(count);
+//     console.log(num);
+//     count++;
+// }
