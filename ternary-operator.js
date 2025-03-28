19d0890f90c2cb2cafe382a806fb10f035bf926a
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-28 21:50:49 +0600

    ternary operator

---
+++ ternary-operator.js
@@ -0,0 +1,18 @@
+
+// let age = 16;
+
+// let vote = (age >= 18) ? 'vote dite parbo' : 'vote dite parbo na';
+// console.log(vote)
+
+// let a = 8;
+// ;
+// let b;
+// let c;
+// (a >= 5) ? b = '5 theke boro': c = '5 theke choto';
+
+// if(b !== undefined){
+//     console.log(b);
+// }
+// else if(c !== undefined){
+//     console.log(c)
+// }
