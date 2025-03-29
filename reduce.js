17fd61b14eff316e69a2b7e29a14a74b28788188
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-29 23:14:57 +0600

    reduce

---
+++ reduce.js
@@ -0,0 +1,30 @@
+// const numbers = [1,2,3,4,5];
+
+// const result = numbers.reduce((previousValue, currentValue) => {
+//     return previousValue * currentValue;
+// }, 1)
+// console.log(result)
+
+
+// const products = [
+//     {name: "watch", price: 500},
+//     {name: "laptop", price: 1000},
+//     {name: "phone", price: 500}
+// ]
+
+// const x = products.reduce((previousValue, currentValue) => {
+//     return previousValue + currentValue.price;
+// }, 0)
+
+// console.log(x)
+
+// const products = [
+//     {name: "watch", price: 100},
+//     {name: "laptop", price: 1000},
+//     {name: "phone", price: 500}
+// ]
+
+// const totalPrice = products.reduce((total, product) => {
+//     return total + product.price;
+// }, 0)
+// console.log(totalPrice)
