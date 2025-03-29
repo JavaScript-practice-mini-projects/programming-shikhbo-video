1c7aa328e3804dfa7820c2a1203351a8febed9eb
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-29 22:51:21 +0600

    filter

---
+++ filter.js
@@ -0,0 +1,36 @@
+// let nums = [1,2,3,5,6,7,8,9,10,18,4,17];
+
+// const filterNum = nums.filter((num) => {
+//     return num > 5;
+// })
+
+// // console.log(filterNum)
+
+// let numbers = [1,2,3,5,6,7,8,9,10,18,4,17];
+
+// const flnum = numbers.map((num) => {
+//     return num > 5;
+// })
+
+// console.log(flnum)
+
+// const array = [
+//     { name: "John", age: 25, city: "Dhaka" },
+//     { name: "Alice", age: 28, city: "Chittagong" },
+//     { name: "Bob", age: 23, city: "Khulna" },
+//     { name: "John", age: 25, city: "Dhaka" },
+//     { name: "Eve", age: 30, city: "Rajshahi" },
+//     { name: "Charlie", age: 35, city: "Sylhet" },
+//     { name: "Alice", age: 28, city: "Chittagong" },
+//     { name: "David", age: 40, city: "Barisal" },
+//     { name: "Frank", age: 25, city: "Dhaka" },
+//     { name: "Grace", age: 22, city: "Comilla" },
+//     { name: "Bob", age: 23, city: "Khulna" },
+//     { name: "Helen", age: 30, city: "Rajshahi" },
+// ];
+
+// const filterResult = array.filter((arr) => {
+//     return arr.age > 30;
+// })
+
+// console.log(filterResult)
