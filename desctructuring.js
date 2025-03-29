033d42e9cd7c6d437e0038fba8a58529ff328121
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-29 23:39:35 +0600

    destructuring

---
+++ desctructuring.js
@@ -0,0 +1,13 @@
+// const me = {
+//     nam: "habib",
+//     age: 23,
+//     skill: "developer",
+//     more: {
+//         profession: 'programmer',
+//         ssc: 'A+'
+//     }
+// }
+
+// const {nam, age, skill, more} = me;
+// const {profession,ssc} = more
+// console.log(nam,age,skill, profession, ssc)
