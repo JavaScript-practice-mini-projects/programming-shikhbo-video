6a8a05ea33e3b721a70d4507179c66b44c165a98
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-29 22:16:32 +0600

    forEach loop

---
+++ forEach.js
@@ -0,0 +1,7 @@
+// const  fvPerson = ['rupa', 'ontora', 'nilima', 'sathi', 'fency'];
+
+// fvPerson.forEach(myfunc);
+
+// function myfunc(value){
+//     console.log(value);
+// }
---
+++ idia.js
@@ -0,0 +1,15 @@
+// let love = true; 
+
+// while (true) {
+//     if(love){
+//         console.error("Life is running with errors...");
+//     }  
+//     if(!love){
+//         console.log("Life is running smoothly!");
+//         break;
+//     }
+// }
+
+
+
+
