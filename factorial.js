22a00557e09ddc390acd03c06fd33d2565ae687d
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-30 17:09:49 +0600

    factorial

---
+++ factorial.js
@@ -0,0 +1,23 @@
+// let nums = [1,2,3,4,5];
+// const x = nums.reduce((p,c) => {
+//     return p + c;
+// }, 0)
+// console.log(x)
+
+// let sum = 0;
+// for(let i = 1; i <= 4; i++){
+//     sum = sum + i;
+//     console.log(sum)
+// }
+
+// let sum = 1;
+// for(let i = 0; i < 4; i++){
+//     sum = sum + i;
+//     console.log(sum)
+// }
+
+// let multi = 1;
+// for(let i = 1; i <= 4; i++){
+//     multi = multi * i;
+//     console.log(multi)
+// }
