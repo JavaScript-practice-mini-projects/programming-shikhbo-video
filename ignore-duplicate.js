2210de690960badd0cb9ae797e59d25ef5bf7b0a
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-30 22:22:39 +0600

    includes

---
+++ ignore-duplicate.js
@@ -0,0 +1,17 @@
+// const friends = ["Rahim", "Karim", "Sakib", "Rahim", "Jabir", "Sakib", "Fahim", "Karim", "Sakib"];
+
+
+// function ignore(friends){
+//     let friendList = [];
+//     for(let i = 0; i < friends.length; i++){
+//         const name = friends[i];
+//         if(friendList.includes(name) !== true){
+//             friendList.push(name);
+//         }
+//     }
+//     console.log(friendList)
+// }
+
+// ignore(friends)
+
+
