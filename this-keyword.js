f09f076f97c1f14da485b358890b93bc7da0d076
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-30 22:40:00 +0600

    this key

---
+++ this-keyword.js
@@ -0,0 +1,10 @@
+
+// const user = {
+//     fName: 'md',
+//     lName: 'habib',
+//     info (){
+//         contact: 'habib@example.com';
+//         phone: '93948'
+//     }
+// }
+// console.log(user.info)
