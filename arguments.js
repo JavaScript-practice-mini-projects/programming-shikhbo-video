2056ce39b05ab4ec35ad3adc2b9f444607fa936d
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-30 18:02:23 +0600

    arguments

---
+++ arguments.js
@@ -0,0 +1,34 @@
+// function sum(...num){
+//     num.forEach((x) => {
+//         console.log(x)
+//     })
+//     console.log(num)
+// }
+
+// sum(5,2,3,54)
+
+// function sum(){
+//     console.log(arguments.length)
+// }
+// sum(1,2,3,4,5,6)
+
+
+// function sum(){
+//     console.log(arguments)
+//     let numSum = 0;
+//     arguments.forEach((num) => {
+//         numSum = numSum + num;
+//     })
+// }
+
+// sum(1,2,3,4)
+
+// function sum(){
+//     let sumNum = 0;
+//     for(let i = 0; i < arguments.length; i++){
+//         sumNum = sumNum + arguments[i];
+//     }
+//     return sumNum;
+// }
+
+// console.log(sum(5,5,5,5))
