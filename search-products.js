5287a87d09967cd89762c563415865420efe49f4
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-30 23:18:33 +0600

    products search

---
+++ search-products.js
@@ -0,0 +1,30 @@
+// const products = [
+//     { id: 1, pName: "Laptop", price: 75000 },
+//     { id: 2, pName: "Smartphone", price: 25000 },
+//     { id: 3, pName: "Headphone", price: 3000 },
+//     { id: 4, pName: "Smartwatch", price: 5000 },
+//     { id: 5, pName: "Keyboard", price: 1500 },
+//     { id: 6, pName: "Mouse", price: 1200 },
+//     { id: 7, pName: "Monitor", price: 15000 },
+//     { id: 8, pName: "Printer", price: 20000 },
+//     { id: 9, pName: "Tablet", price: 30000 },
+//     { id: 10, pName: "Webcam", price: 4000 },
+//     { id: 11, pName: "Router", price: 3500 },
+//     { id: 12, pName: "Speaker", price: 5000 },
+//     { id: 13, pName: "External Hard Drive", price: 8000 },
+//     { id: 14, pName: "Graphics Card", price: 45000 },
+//     { id: 15, pName: "Microphone", price: 7000 }
+// ];
+  
+// const searchProducts = (products,searchProduct) => {
+//     let searchList = [];
+//     for(const product of products){
+//         if(product.pName.toLowerCase().includes(searchProduct.toLowerCase())){
+//             searchList.push(product)
+//         }
+//     }
+//       console.log(searchList)  
+// }
+
+// searchProducts(products, 'Microphone')
+  
