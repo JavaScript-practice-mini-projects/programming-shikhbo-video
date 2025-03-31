efbfa02f7cd0a94ff38889595ce523a2b654c7a2
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-31 14:02:00 +0600

    dynamic data fetch

---
+++ API/user/user.js
@@ -0,0 +1,25 @@
+// const container = document.getElementById('container');
+
+// const div = document.createElement('div');
+// const img = document.createElement('img');
+// // img.src 
+// div.appendChild(img);
+// container.appendChild(div);
+
+// function getData(){
+//     fetch('https://picsum.photos/v2/list?page=1&limit=10')
+//     .then(Response => Response.json())
+//     .then(data => dom(data))
+// }
+// getData()
+// function dom(data){
+//     for( const photo of data){
+//         const container = document.getElementById('container');
+//         const div = document.createElement('div');
+//         div.classList.add('user')
+//         const img = document.createElement('img');
+//         img.src =  photo.download_url
+//         div.appendChild(img);
+//         container.appendChild(div);
+//     }
+// }
