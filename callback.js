8ec74b0d441537de42b09dd0b962035f20c41a14
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-31 11:55:07 +0600

    callback function

---
+++ callback.js
@@ -0,0 +1,33 @@
+// console.log('print1')
+// setTimeout(() => {
+//     console.log('print2')
+// }, 2000)
+// console.log('print3')
+
+// const first = (a,b, callback) =>{
+//     setTimeout(() => {
+//         console.log('first:', a + b);
+//         callback()
+//     }, 2000);
+// }
+
+// const second = () =>{
+//     console.log('second');
+// }
+
+// first(2,4, second)
+
+
+// const first = () =>{
+//     console.log('first')
+// }
+
+// const second = () =>{
+//     console.log('second');
+// }
+
+
+// setTimeout(() => {
+//     first()
+//     second()
+// }, 2000);
