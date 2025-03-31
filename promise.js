85ea102210dbe786a5bf5ce6e060cfbcbafd11cf
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-31 12:19:24 +0600

    promise

---
+++ promise.js
@@ -0,0 +1,28 @@
+
+// promise: 
+/**
+ *  1. Pending
+ *  2. Resolve
+ *  3. Reject
+ */
+
+// new Promise()  //Pending
+
+
+// let love = true;
+// //work on params: Resolve or Reject.
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if(love){
+//             resolve('Yes, Your are my earth');
+//         }
+//         else{
+//             reject('Your are hell for my live');
+//         }
+//     }, 2000);
+// }).then((message) => { //if true or Resolve then push data here.
+//     console.log(message);
+// }).catch((error) => { //if false or Reject then push data here.
+//     console.log(error);
+// })
+
